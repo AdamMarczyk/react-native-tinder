9e1b960bc071bad2df1ab5bf19f5038e4445a46a
Adam Marczyk <adam.marczyk90@gmail.com>
2018-08-06 11:58:55 +0200

    reset card position

--- src/Deck.js
+++ src/Deck.js
@@ -6,6 +6,11 @@ const SWIPE_THRESHOLD = 0.25 * SCREEN_WIDTH;
 const SWIPE_OUT_DURATION = 250;
 
 class Deck extends Component {
+  static defaultProps = {
+    onSwipeRight: () => {},
+    onSwipeLeft: () => {}
+  };
+
   constructor(props) {
     super(props);
 
@@ -28,7 +33,8 @@ class Deck extends Component {
 
     this.state = {
       panResponder,
-      position
+      position,
+      index: 0
     };
   }
 
@@ -37,7 +43,16 @@ class Deck extends Component {
     Animated.timing(this.state.position, {
       toValue: { x, y: 0 },
       duration: SWIPE_OUT_DURATION
-    }).start();
+    }).start(() => this.onSwipeComplete(direction));
+  }
+
+  onSwipeComplete(direction) {
+    const { onSwipeLeft, onSwipeRight, data } = this.props;
+    const item = data[this.state.index];
+
+    direction == 'right' ? onSwipeRight(item) : onSwipeLeft(item);
+    this.state.position.setValue({ x: 0, y: 0 });
+    this.setState({ index: this.state.index + 1 });
   }
 
   resetPosition() {
